80d8f4c064a45d6a4abbb1734c01bcd23de3ae17
cityuser <morris@schussler.se>
2020-10-25 03:53:34 +0100

    Method rewrite + new methods
    Now written as sorted() methods with lambda functions as keys, which return the row or seat value (or both). Also added new methods: WMA, WMA_BTF, WMA_BTF_ALT

--- methods.py
+++ methods.py
@@ -1,18 +1,26 @@
 # methods.py
 # Contains the sorting functions for the various boarding methods
 
-def backToFront(manifest, rows):
-    sorted = []
-    for row in range(1, rows+1):
-        for passenger in manifest:
-            if passenger.gr == row:
-                sorted.insert(0, passenger)
-    return sorted
+def random(manifest):
+    return manifest
 
-def frontToBack(manifest, rows):
-    sorted = backToFront(manifest, rows)
-    sorted.reverse()
-    return sorted
+def BTF(manifest):
+    return sorted(manifest, key = lambda p: -p.gr)
+
+def FTB(manifest):
+    return sorted(manifest, key = lambda p: p.gr)
+
+def WMA(manifest):
+    return sorted(manifest, key = lambda p: -abs(p.gs))
+
+def WMA_BTF(manifest): # Window Middle Aisle, Back To Front (3 or -3, 2 or -2, 1 or -1)
+    return sorted(manifest, key = lambda p: (-abs(p.gs), -p.gr))
+
+def WMA_BTF_ALT(manifest): # Window Middle Aisle, Back To Front, Alternating sides. (3, -3, 2, -2, 1, -1)
+    return sorted(manifest, key = lambda p: (-abs(p.gs + 0.25) + 0.25, -p.gr))
+    # -abs(x + 0.25) + 0.25 gives
+        #-abs(x) behaviour for x > -0.25
+        #-abs(x + 0.5) behaviour for x < -0.25
 
 def perfectSteffen():
     return
